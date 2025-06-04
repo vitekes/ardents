7a98d4b4f690b099aca38f7bbb42933360d2c26e
vitekes <vitekes@gmail.com>
2025-06-04 14:40:33 +0300

    init

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -41,7 +41,7 @@ export const authOptions: NextAuthOptions = {
     }),
   ],
   session: {
-    strategy: "database",
+    strategy: "jwt",
   },
   callbacks: {
     async session({ session, user }) {
